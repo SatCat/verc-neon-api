94ddab343988e9ebfb18de6db1f13d08e05fca14
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 15:22:12 +1100

    ping

--- api/api.ts
+++
@@ -1,8 +0,0 @@
-const VER = "0.0.1";
-
-export default (req: Request) => {
-	const headers = JSON.stringify(req.headers);
-
-	
-	return new Response(`PING-API<br>Headers: ${headers}<br>Method: ${req.method}<br>Url:${req.url}`);
-};
