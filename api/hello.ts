19f05b267552f370f94a682744d77cb68f1c9718
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 14:41:47 +1100

    add ver2

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,5 @@
+const VER = "0.0.1";
+
 export default (req: Request) => {
-	return new Response(`Hello, from Deno v${Deno.version.deno}!`);
+	return new Response(`Hello, from Deno v${Deno.version.deno}!   app ver.${VER}`);
 };
