fc2ffe8e205d7cbb80a657cd0d570ad4c4f19381
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 17:14:52 +1100

    json

--- api/api_get_all_batch_ts.ts
+++ api/api_get_all_batch_ts.ts
@@ -18,7 +18,7 @@ export default async (req: Request) => {
         */
         const retval = await sql`SELECT CONCAT(TO_CHAR(batch_ts, 'YYYY-MM-DD'), 'T', TO_CHAR(batch_ts, 'HH24:MI:SS+00:00')) as batch, count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
         await sql.close();
-        return new Response(retval);
+        return new Response(  JSON.stringify(retval)  );
     }
 	return new Response("API Error");
 };
