978680509132de74cf56faa73ff7b9a6ffe55d0d
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 14:58:20 +1100

    addurl

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ const VER = "0.0.1";
 
 export default (req: Request) => {
 	const headers = JSON.stringify(req.headers);
-	return new Response(`Headers: ${headers}<br>Method: ${req.method}`);
+	return new Response(`Headers: ${headers}<br>Method: ${req.method}<br>Url:$(req.url)`);
 };
