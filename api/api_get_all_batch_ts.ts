c208de7cfa123bf0e5920bbd39dfdac8c6e16f3a
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 16:43:50 +1100

    simplifyDateFormat

--- api/api_get_all_batch_ts.ts
+++ api/api_get_all_batch_ts.ts
@@ -7,13 +7,29 @@ type Sql = ReturnType<typeof postgres>;
 
 const VERC_NEON_DB_URL = Deno.env.get("VERC_NEON_DB_URL")
 
+function simplifyDateFormat(items: { batch_ts: Date; count: string }[]): { batch_ts: string; count: string }[] {
+    return items.map(item => {
+      const dateStr = new Date(item.batch_ts).toISOString();
+  
+      // Меняем 'T' на пробел и 'Z' на '+00:00'
+      const formatted = dateStr
+        .replace('T', ' ')
+        .replace('Z', '+00:00');
+  
+      return {
+        ...item,
+        batch_ts: formatted
+      };
+    });
+}
+
 export default async (req: Request) => {
     if (req.method === "POST") {
         const sql: Sql = postgres(VERC_NEON_DB_URL);
-        const get_all_batch_ts = JSON.stringify(await sql`SELECT batch_ts, count(*) FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`);
-
+        const get_all_batch_ts = await sql`SELECT batch_ts, count(*) FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
+        const retval = JSON.stringify(simplifyDateFormat(get_all_batch_ts))
         await sql.close();
-        return new Response(get_all_batch_ts);
+        return new Response(retval);
     }
 	return new Response("API Error");
 };
