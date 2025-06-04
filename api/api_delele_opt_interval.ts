2f8bb47c7bb547bc4e4f07562ef1a0d4a0cc5fa8
SatCat <dm.alyoshin@yandex.ru>
2025-06-05 10:27:51 +1100

    if api

--- api/api_delele_opt_interval.ts
+++ api/api_delele_opt_interval.ts
@@ -6,9 +6,10 @@ import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js";
 type Sql = ReturnType<typeof postgres>;
 
 const VERC_NEON_DB_URL = Deno.env.get("VERC_NEON_DB_URL");
+const VERC_API_KEY = Deno.env.get("VERC_API_KEY");
 
 export default async (req: Request) => {
-    if (req.method === "POST") {
+    if (req.method === "POST" && req.headers.get('apikey') === VERC_API_KEY) {
 
         return new Response(JSON.stringify(req.headers.get('apikey')));
 
