9fbcf97007e044a846f8daa90485e43df0ba81a8
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 17:11:04 +1100

    concat

--- api/api_get_all_batch_ts.ts
+++ api/api_get_all_batch_ts.ts
@@ -7,24 +7,10 @@ type Sql = ReturnType<typeof postgres>;
 
 const VERC_NEON_DB_URL = Deno.env.get("VERC_NEON_DB_URL")
 
-function simplifyDateFormat(items) {
-    return items.map(item => {
-      const dateStr = new Date(item.batch_ts).toISOString();
-      const formatted = dateStr
-        .replace(/\.\d{3}Z$/, 'Z')
-        .replace('Z', '+00:00');
-  
-      return {
-        batch: formatted,
-        count: item.count
-      };
-    });
-  }
-
 export default async (req: Request) => {
     if (req.method === "POST") {
         const sql: Sql = postgres(VERC_NEON_DB_URL);
-        const get_all_batch_ts = await sql`SELECT batch_ts::TEXT, count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
+        const get_all_batch_ts = await sql`SELECT CONCAT(TO_CHAR(batch_ts, 'YYYY-MM-DD'), 'T', TO_CHAR(batch_ts, 'HH24:MI:SS+00:00')), count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
         const retval = JSON.stringify(get_all_batch_ts)
         await sql.close();
         return new Response(retval);
