a2d9b9d2d202f85bc8a14e92ee3a6174b8238eeb
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 15:00:12 +1100

    ch brack

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,7 @@ const VER = "0.0.1";
 
 export default (req: Request) => {
 	const headers = JSON.stringify(req.headers);
-	return new Response(`Headers: ${headers}<br>Method: ${req.method}<br>Url:$(req.url)`);
+
+	
+	return new Response(`Headers: ${headers}<br>Method: ${req.method}<br>Url:${req.url}`);
 };
