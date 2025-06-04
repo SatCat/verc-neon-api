1ed51d0c0771ca1f7840b26688a18c75497674f0
SatCat <dm.alyoshin@yandex.ru>
2025-06-05 09:50:23 +1100

    add api_delele_opt_interval

--- api/api_delele_opt_interval.ts
+++ api/api_delele_opt_interval.ts
@@ -1,5 +1,5 @@
 //
-//                  api_get_all_batch_ts
+//                  api_delele_opt_interval
 //
 
 import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js"; 
@@ -9,10 +9,16 @@ const VERC_NEON_DB_URL = Deno.env.get("VERC_NEON_DB_URL")
 
 export default async (req: Request) => {
     if (req.method === "POST") {
+
+        return new Response(JSON.stringify(req.headers))
+
+
+        /*
         const sql: Sql = postgres(VERC_NEON_DB_URL);
         const retval = await sql`SELECT CONCAT(TO_CHAR(batch_ts, 'YYYY-MM-DD'), 'T', TO_CHAR(batch_ts, 'HH24:MI:SS+00:00')) as batch, count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
         await sql.close();
         return new Response(  JSON.stringify(retval)  );
+        */
     }
-	return new Response("API Error");
+    return new Response("API Error");
 };
