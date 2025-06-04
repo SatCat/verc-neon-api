c0a9af4a8669935a60df95cbe1d3e5aa1d2cb3f9
SatCat <dm.alyoshin@yandex.ru>
2025-06-04 16:02:04 +1100

    get_all_batch_ts

--- api/api_get_all_batch_ts.ts
+++ api/api_get_all_batch_ts.ts
@@ -1,3 +1,7 @@
+//
+//                  api_get_all_batch_ts
+//
+
 import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js"; 
 type Sql = ReturnType<typeof postgres>;
 
@@ -5,12 +9,12 @@ const VERC_NEON_DB_URL = Deno.env.get("VERC_NEON_DB_URL")
 
 export default async (req: Request) => {
     if (req.method === "POST") {
-        // нужно запросить из базы все batch_ts
         const sql: Sql = postgres(VERC_NEON_DB_URL);
-        const get_time = JSON.stringify(await sql`select now();`);
-        await sql.close();
+        const get_all_batch_ts = JSON.stringify(await sql`SELECT batch_ts, count(*) FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`);
 
-        return new Response(`SQL result: ${get_time}`);
+
+        await sql.close();
+        return new Response(`SQL result: ${get_all_batch_ts}`);
     }
 	return new Response("API Error");
 };
