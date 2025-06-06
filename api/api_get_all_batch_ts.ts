7f8c7f4f45494ae3a916bff783c6e5bbeeb22910
SatCat <dm.alyoshin@yandex.ru>
2025-06-06 14:57:45 +1100

    add func

--- api/api_get_all_batch_ts.ts
+++ api/api_get_all_batch_ts.ts
@@ -11,9 +11,29 @@ const VERC_API_KEY = Deno.env.get("VERC_API_KEY");
 export default async (req: Request) => {
     if (req.method === "POST" && req.headers.get('apikey') === VERC_API_KEY) {
         const sql: Sql = postgres(VERC_NEON_DB_URL);
-        const retval = await sql`SELECT CONCAT(TO_CHAR(batch_ts, 'YYYY-MM-DD'), 'T', TO_CHAR(batch_ts, 'HH24:MI:SS+00:00')) as batch, count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
+        //const retval = await sql`SELECT CONCAT(TO_CHAR(batch_ts, 'YYYY-MM-DD'), 'T', TO_CHAR(batch_ts, 'HH24:MI:SS+00:00')) as batch, count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
+        const retval = await sql`SELECT public.TPZZ(batch_ts) as batch, count(*)::INTEGER FROM public.all_options GROUP BY batch_ts ORDER BY batch_ts ASC;`;
         await sql.close();
         return new Response(  JSON.stringify(retval)  );
     }
 	return new Response("API Error");
 };
+
+/*
+  В бд есть дву функции, которые выдают строки timestamptz в правильном формате (без ms и с ms)
+--DROP FUNCTION public.TPZZ(timestamptz);
+CREATE OR REPLACE FUNCTION public.TPZZ(d timestamptz)
+RETURNS TEXT AS $$
+BEGIN
+  RETURN CONCAT(TO_CHAR(d, 'YYYY-MM-DD'), 'T', TO_CHAR(d, 'HH24:MI:SS+00:00'));
+END;
+$$ LANGUAGE plpgsql;
+
+--DROP FUNCTION public.TPZZ_ms(timestamptz);
+CREATE OR REPLACE FUNCTION public.TPZZ_ms(d timestamptz)
+RETURNS TEXT AS $$
+BEGIN
+  RETURN CONCAT(TO_CHAR(d, 'YYYY-MM-DD'), 'T', TO_CHAR(d, 'HH24:MI:SS.MS+00:00'));
+END;
+$$ LANGUAGE plpgsql;
+*/
