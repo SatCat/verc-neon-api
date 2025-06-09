ab56129751a797d6dfc208e3f5916e0096d1b0cb
SatCat <dm.alyoshin@yandex.ru>
2025-06-10 09:08:34 +1100

    VACUUM FULL

--- api/api_get_opt_interval.ts
+++ api/api_get_opt_interval.ts
@@ -15,7 +15,7 @@ export default async (req: Request) => {
     if (req.method === "POST" && req.headers.get('apikey') === VERC_API_KEY) {
         //  input format!!:  { "batch_from": "2025-06-08 00:00:00+00:00",  "batch_to": "2025-06-08 00:55:00+00:00"}
         const {batch_from, batch_to} = await req.json() as Datas_from_to;
-
+        console.log()
         const sql: Sql = postgres(VERC_NEON_DB_URL);
         const retval = await sql`SELECT instrument_name, option_type, strike, public.TPZZ(expiration_timestamp) expiration_timestamp, public.TPZZ(creation_timestamp) creation_timestamp,
             public.TPZZ_ms(timestamp) timestamp, index_price, underlying_index, underlying_price, underlying_price, open_interest, mark_price, best_ask_price, best_bid_price, mark_iv, delta, 
